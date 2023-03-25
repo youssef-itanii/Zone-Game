c08b988e1aa9f0a8bd98369c939e925a2ada0085
itaniy <youssef.itani@gmail.com>
2023-03-25 10:55:57 +0100

    added missing function

--- src/shared/remote_objects/IManager.java
+++ src/shared/remote_objects/IManager.java
@@ -7,6 +7,7 @@ import shared.common.Message;
 public interface IManager extends Remote{
 
 	void register(IClient client) throws RemoteException;
+	void register(IZone zone) throws RemoteException;
 	void unregister(IClient client) throws RemoteException;
 	void moveClient(IClient client, IZone dest) throws RemoteException;
 	void sendMessage(IClient client , Message message) throws RemoteException;
