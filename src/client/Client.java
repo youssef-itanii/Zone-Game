10623806120988c55bdbe5804f06c537acc635f0
itaniy <youssef.itani@gmail.com>
2023-03-25 11:12:00 +0100

    added CLIMEssage class

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,7 +1,56 @@
 package client;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import manager.Manager;
+import shared.common.Message;
 import shared.remote_objects.IClient;
+import shared.remote_objects.IZone;
+import zone.Zone;
 
 public class Client implements IClient{
+
+	private Manager connection;
+	private Zone subscribedZone;
+	
+	public void User() {
+		try {
+			UnicastRemoteObject.exportObject(this , 0);
+		}
+		catch(RemoteException e) {
+			
+		}
+	}
+	
+	@Override
+	public void setZone(IZone target) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCoordinates(int x, int y) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getX() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getY() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recieveMessage(Message message) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
---
+++ src/shared/common/CLIMessage.java
@@ -0,0 +1,9 @@
+package shared.common;
+
+public class CLIMessage {
+	public static void DisplayMessage(String message , Boolean isError) {
+		
+	}
+	
+
+}
